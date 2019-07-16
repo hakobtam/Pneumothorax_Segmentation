a0b6ecd5cf0c498b59e8969ad4de42dcf2846e7e
Grisha Zohrabyan <grishazohrabyan@gmail.com>
2019-07-16 20:44:46 +0400

    Last changes

--- data_processor/pneumothorax_dataset.py
+++ data_processor/pneumothorax_dataset.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 import os
 import pydicom
+import PIL
 
 
 class PneumothoraxDataset(Dataset):
-    def __init__(self, dcm_images_dir, mask_csv_path):
+    def __init__(self, dcm_images_dir, mask_csv_path, reshape=128):
         self.dcm_images_dir = dcm_images_dir
         self.mask_csv_path = mask_csv_path
 
@@ -15,6 +16,8 @@ class PneumothoraxDataset(Dataset):
         self.masks = {row["ImageId"]: row[" EncodedPixels"] for row in self.mask_df.to_dict('records')}
 
         self.dcm_images_names = os.listdir(self.dcm_images_dir)
+
+        self.reshape = reshape
 
     def __len__(self):
         return len(self.dcm_images_names)
@@ -30,16 +33,19 @@ class PneumothoraxDataset(Dataset):
 
         shape_1 = data.Rows
         shape_2 = data.Columns
+        img = PIL.Image.fromarray(data.pixel_array).resize((self.reshape, self.reshape))
+        mask = PIL.Image.fromarray(self.rle2mask(self.masks[img_name.replace(".dcm", "")],
+                                                 shape_1, shape_2)).resize((self.reshape, self.reshape))
 
-        result = {"img": data.pixel_array, "patient_id": data.PatientID, "age": data.PatientAge, "sex": data.PatientSex,
+        result = {"img": img, "patient_id": data.PatientID, "age": data.PatientAge, "sex": data.PatientSex,
                   "modality": data.Modality, "body_part": data.BodyPartExamined, "position": data.ViewPosition,
-                  "mask": self.rle2mask(self.masks[img_name.replace(".dcm", "")], shape_1, shape_2)}
+                  "mask": mask}
 
         return result
 
     @staticmethod
     def rle2mask(rle, width, height):
-        if rle == '-1':
+        if rle.strip() == '-1':
             return np.zeros((width, height))
         mask = np.zeros(width * height)
         array = np.asarray([int(x) for x in rle.split()])
---
+++ main.py
@@ -0,0 +1,47 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+import torch.nn.functional as F
+from torch import nn
+
+
+from data_processor import PneumothoraxDataset
+from train import train_model, eval_model
+from models import UNet
+
+dcm_train_dir = r"C:\Users\Grisha Zohrabyan\dev\Kaggle\pneumothorax_data\images-train"
+dcm_test_dir = r"C:\Users\Grisha Zohrabyan\dev\Kaggle\pneumothorax_data\images-test"
+masks_csv = r"C:\Users\Grisha Zohrabyan\dev\Kaggle\pneumothorax_data\train-rle.csv"
+
+train_set = PneumothoraxDataset(dcm_images_dir=dcm_train_dir, mask_csv_path=masks_csv)
+test_set = PneumothoraxDataset(dcm_images_dir=dcm_test_dir, mask_csv_path=masks_csv)
+
+epochs = 10
+batch_size = 32
+learning_rate = 0.001
+shuffle = True
+num_workers = 1
+
+train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+model = UNet(1, 1)
+optimizer = Adam(params=model.parameters(), lr=learning_rate)
+
+
+def rmse(y, y_hat):
+    """Compute root mean squared error"""
+    return torch.sqrt(torch.mean((y.double() - y_hat.double()).pow(2)))
+
+
+def rle_loss(y, y_hat):
+
+    def array2rle(arr):
+        pass
+
+
+
+loss = rmse
+
+if __name__ == '__main__':
+    for e in range(epochs):
+        train_model(model=model, optimizer=optimizer, data=train_dataloader, loss_fn=loss, epoch=e)
+
