ddae6dbf6c2e50d1ef7bf9ed52bf611e34d7a032
hakobtam <tam.hak27@gmail.com>
2019-07-24 19:53:43 +0000

    change gitignore

--- runs/Jul24_19-13-40/command.sh
+++
@@ -1,1 +0,0 @@
-train.py --model UNet --num_filters 32 --folds_dir 10folds --fold_id 0 --img_size 512 --num_workers 4 --batch_size 16 --loss Loss --wd 1e-4 --optim adam --grad_accumulation 1 --lr 1e-2 --epochs 100 --seed 27 --debug
